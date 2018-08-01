f0d884573f7c6e79e555c5a7c91846e4dd6a7d7d
Knights of the Force 2.1 <linken233@hotmail.com>
2018-08-01 15:19:12 -0400

    Revert "[android] fixed never saving favorite servers"
    This reverts commit e0bf42ee09d8121571d53df9bb78d9bb991ea79c.

--- codemp/ui/ui_main.c
+++ codemp/ui/ui_main.c
@@ -1099,9 +1099,7 @@ _UI_Shutdown
 void UI_CleanupGhoul2(void);
 
 void _UI_Shutdown( void ) {
-#ifdef __ANDROID__
-	trap_LAN_SaveCachedServers();
-#endif
+//	trap_LAN_SaveCachedServers();
 	UI_CleanupGhoul2();
 }
 
@@ -10374,9 +10372,8 @@ void _UI_Init( qboolean inGameLoad ) {
 
 
 	Menus_CloseAll();
-#ifdef __ANDROID__
-	trap_LAN_LoadCachedServers();
-#endif
+
+//	trap_LAN_LoadCachedServers();
 	UI_LoadBestScores(uiInfo.mapList[ui_currentMap.integer].mapLoadName, uiInfo.gameTypes[ui_gameType.integer].gtEnum);
 
 	UI_BuildQ3Model_List();
