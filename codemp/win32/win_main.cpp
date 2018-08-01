b3526ca8b58c4205766772d6480aeb4524c7cdf8
Knights of the Force 2.1 <linken233@hotmail.com>
2018-08-01 15:18:50 -0400

    Revert "made externally opening demos and ja:// protocol get opened in the currently running instance"
    This reverts commit ca842e3dba8f732034d078d9f89f1e19e305b0fd.

--- codemp/win32/win_main.cpp
+++ codemp/win32/win_main.cpp
@@ -17,8 +17,6 @@
 
 #define MEM_THRESHOLD 128*1024*1024
 
-UINT MSH_BROADCASTARGS;
-
 //static char		sys_cmdline[MAX_STRING_CHARS];
 
 /* win_shared.cpp */
@@ -1321,17 +1319,6 @@ void RegisterFileTypes(char *program) {
 	}
 }
 
-static HANDLE mutex;
-bool isOtherInstanceRunning(void) {
-	mutex = CreateMutex(NULL, TRUE, PROGRAM_MUTEX);
-	//prevent multiple instances if we are opening a demo
-	if (ERROR_ALREADY_EXISTS == GetLastError()) {
-		return true;
-	}
-	//in the other case just open another sintance
-	return false;
-}
-
 int main( int argc, char **argv )
 {
 	int		i;
@@ -1372,7 +1359,6 @@ int main( int argc, char **argv )
 		RegisterFileTypes(path);
 		RegisterProtocol(path);
 	}
-	Com_Printf("MSH_BROADCASTARGS: %u\n", MSH_BROADCASTARGS);
 
 #if !defined(DEDICATED)
 	QuickMemTest();
@@ -1430,21 +1416,6 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 		MessageBox(NULL, "Out of memory - aborting", "Fatal Error", MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
-	MSH_BROADCASTARGS = RegisterWindowMessage("MSH_BROADCASTARGS");
-	if (isOtherInstanceRunning()) {
-		char *bcArgs = strstr(cmdline, "+demo");
-		if (!bcArgs)
-			bcArgs = strstr(cmdline, "ja:");
-		if (bcArgs) {
-			Sys_CopySharedData(bcArgs, strlen(bcArgs)+1);
-			PostMessage(HWND_BROADCAST, MSH_BROADCASTARGS, 0, 0);
-			Sleep(1337);
-			free(cmdline);
-			ReleaseMutex(mutex);
-			CloseHandle(mutex);
-			return 0;
-		}
-	}
 
 	int    argc = ParseCommandLine(cmdline, NULL);
 	char **argv = (char **)alloca(sizeof(char *) * argc + 1);
@@ -1463,9 +1434,6 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 	main(argc, argv);
 
 	free(cmdline);
-
-	ReleaseMutex(mutex);
-	CloseHandle(mutex);
 
 	/* End Sam Lantinga Public Domain 4/13/98 */
 
